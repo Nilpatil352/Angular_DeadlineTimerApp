5e6bd5b233fb9411af01c8254234c3c732f23c3e
Nilpatil352 <51432275+Nilpatil352@users.noreply.github.com>
2026-01-02 17:42:03 +0530

    route added for deadline timer component

--- DeadlineTimerApp/src/app/app-routing.module.ts
+++ DeadlineTimerApp/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ParentComponent } from './components/parent/parent.component';
+import { DeadlineTimerComponent } from './components/deadline-timer/deadline-timer.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'deadline', component: DeadlineTimerComponent }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
